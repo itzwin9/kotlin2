1f974a379703c4227fd8e4e8457f413952c000d4
itzwin9 <hamsterplay09@gmail.com>
2021-04-24 15:34:39 +0700

    aboba

---
+++ src/hello.kt
@@ -0,0 +1,30 @@
+fun main(args: Array<String>) {
+ val aboba = arrayOf("1, 2, 3 ",1, 2, 3)
+    println(aboba[0])
+    val x: Int = 1
+    println(x)
+    val pi: Float = 3.14f
+    println(pi)
+    val d: Double = 3.142
+    println(d)
+    val t: Boolean = true
+    println(t)
+    val martinLutherQuote: String = "j"
+    println(martinLutherQuote)
+    val c: Char = 'j'
+    println(c)
+
+
+   for (key in martinLutherQuote){
+      print(key)
+   }
+
+
+
+   println()
+
+   for (key in martinLutherQuote){
+      print(key)
+   }
+
+}
